6cb6589b46c097d926dfcea6d7bf8184f4bb0e2e
sonjaJolanda <80689952+sonjaJolanda@users.noreply.github.com>
2024-05-04 10:09:54 +0200

    initialize

---
+++ pouring_problem_algorithm.py
@@ -0,0 +1,22 @@
+
+
+
+def sort (pitchers):
+    print("-- sorting")
+    return sorted(pitchers)
+
+
+def pour(pitchers):
+    print("-- pouring")
+
+
+
+
+
+
+pitchers = [42, 19, 2]
+print(pitchers)
+pitchers = sort(pitchers)
+print(pitchers)
+pitchers = pour(pitchers)
+
